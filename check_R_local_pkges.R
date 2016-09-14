1ba95a4741df4b7759aaf16aaec307795f462d1e
EYY <ejyang06@gmail.com>
2016-09-14 09:59:11 -0700

    Revert "BenchmarkModel"
    This reverts commit 0f7cb0e20449a51d7cd2dbfc9280b73e9aedce45.

--- check_R_local_pkges.R
+++
@@ -1,8 +0,0 @@
-###########################################
-# Check Packages Installed on your computer
-#########################################
-
-ipack <- as.data.frame(installed.packages()[,c(1,3:4)])
-#rownames(ipack) <- NULL
-ipack <- ipack[is.na(ipack$Priority),1:2,drop=FALSE]
-print(ipack, row.names=FALSE)
